fb5c37b08095a9bb340009979a5a6564cf51c974
vlajs16 <vladimir.belca@gmail.com>
2018-04-20 16:31:32 +0200

    Dodata je logicka kontrola u klasu User

--- TaskManager/src/user/User.java
+++ TaskManager/src/user/User.java
@@ -7,29 +7,70 @@ public class User {
 	private String password;
 	
 	
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
+		if(name.equals(null))
+			throw new RuntimeException("Name is null");
+		if(name.length()<2)
+			throw new RuntimeException("Name is too short");
+		if(!onlyAlpha(name))
+			throw new RuntimeException("Name contains something that is not alphabetic character");
 		this.name = name;
 	}
+	
+	private boolean onlyAlpha(String text) {
+		for(int i = 0; i < text.length(); i++)
+			if(!Character.isAlphabetic(text.charAt(i)) && text.charAt(i) != ' ')
+				return false;
+		return true;
+	}
+	
 	public String getSurname() {
 		return surname;
 	}
 	public void setSurname(String surname) {
+		if(surname.equals(null))
+			throw new RuntimeException("Surname is null");
+		if(surname.length()<2)
+			throw new RuntimeException("Surame is too short");
+		if(!onlyAlpha(surname))
+			throw new RuntimeException("Surame contains something that is not alphabetic character");
 		this.surname = surname;
 	}
+	
 	public String getUsername() {
 		return username;
 	}
+	
 	public void setUsername(String username) {
+		if(username.equals(null))
+			throw new RuntimeException("Surname is null");
+		if(username.length()<2)
+			throw new RuntimeException("Surame is too short");
 		this.username = username;
 	}
 	public String getPassword() {
 		return password;
 	}
 	public void setPassword(String password) {
+		if(password.equals(null))
+			throw new RuntimeException("Password is null");
+		if(password.length()<6)
+			throw new RuntimeException("Password is too short");
+		if(!tryPass(password))
+			throw new RuntimeException("Password does not contain number");
 		this.password = password;
+	}
+	
+	private boolean tryPass(String pass) {
+		for(int i = 0; i<pass.length(); i++) {
+			if(Character.isDigit(pass.charAt(i)))
+				return true;
+		}
+		return false;
 	}
 	
 	
