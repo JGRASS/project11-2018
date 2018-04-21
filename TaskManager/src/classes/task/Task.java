e30a56294fc2b82ca4ae2bce77c159e4ed2f1a6c
vlajs16 <vladimir.belca@gmail.com>
2018-04-21 12:26:20 +0200

    Dodata je logicka kontrola u klasu Task

--- TaskManager/src/classes/task/Task.java
+++ TaskManager/src/classes/task/Task.java
@@ -20,24 +20,42 @@ public class Task {
 		return taskTitle;
 	}
 	public void setTaskTitle(String taskTitle) {
+		if(taskTitle.equals(null))
+			throw new RuntimeException("Task title is null");
+		if(taskTitle.isEmpty())
+			throw new RuntimeException("Task title is empty string");
+		if(taskTitle.length()<10)
+			throw new RuntimeException("Task title is too short");
 		this.taskTitle = taskTitle;
 	}
 	public GregorianCalendar getDate() {
 		return date;
 	}
 	public void setDate(GregorianCalendar date) {
+		if(date.equals(null))
+			throw new RuntimeException("Date is null");
+		if(date.before(new GregorianCalendar()))
+			throw new RuntimeException("You cannot schedule a task in the past");
 		this.date = date;
 	}
 	public CoWorker getCoWorker() {
 		return coWorker;
 	}
 	public void setCoWorker(CoWorker coWorker) {
+		if(coWorker.equals(null))
+			throw new RuntimeException("CoWorker is null");
 		this.coWorker = coWorker;
 	}
 	public String getDescription() {
 		return description;
 	}
 	public void setDescription(String description) {
+		if(description.equals(null))
+			throw new RuntimeException("Description is null");
+		if(description.isEmpty())
+			throw new RuntimeException("Description is empty string");
+		if(description.length()<10)
+			throw new RuntimeException("Description is too short");
 		this.description = description;
 	}
 	public boolean isDone() {
