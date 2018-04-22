fc5442a45c265e2a7e7564aac46dbb432c37a498
vlajs16 <vladimir.belca@gmail.com>
2018-04-22 13:02:38 +0200

    Napravljena klasa SOSortByDate, ali nije implementirana jos uvek

---
+++ TaskManager/src/system_operations/SOSortByDate.java
@@ -0,0 +1,14 @@
+package system_operations;
+
+import java.util.ArrayList;
+
+import classes.task.Task;
+
+public class SOSortByDate {
+	
+	public static ArrayList<Task> execute(ArrayList<Task> tasks){
+		
+		return null;
+	}
+	
+}
