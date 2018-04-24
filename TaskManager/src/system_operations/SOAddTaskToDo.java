cf4d1ac29819bb701239b57ac412235c0d488097
Teodora Acimov <acimov.teodora@gmail.com>
2018-04-24 14:55:48 +0200

    Dokumentovana klasa SOAddTaskToDo

--- TaskManager/src/system_operations/SOAddTaskToDo.java
+++ TaskManager/src/system_operations/SOAddTaskToDo.java
@@ -6,6 +6,13 @@ import classes.task.Task;
 
 public class SOAddTaskToDo {
 	
+	
+	/**
+	 * Dodavanje novog zadatka
+	 * @param task novi zadatak koji dodajemo
+	 * @param tasks trenutna lista zadataka
+	 * @return tasks azurirana lista zadataka
+	 */
 	public static ArrayList<Task> execute(Task task, ArrayList<Task> tasks) {
 		if (!tasks.contains(task))
 			tasks.add(task);
