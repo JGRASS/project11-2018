b2e9a101824658b2c330c07f46f539fd6261b90c
Teodora Acimov <acimov.teodora@gmail.com>
2018-05-06 20:59:15 +0200

    Ispravljen izgled HistoryFrame-a

--- TaskManager/src/gui/HistoryFrame.java
+++ TaskManager/src/gui/HistoryFrame.java
@@ -18,6 +18,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.event.ChangeListener;
 
 import classes.coworker.CoWorker;
+import classes.task.Task;
 import gui.fonts.Fonts;
 import gui.kontroler.GUIKontroler;
 
@@ -32,22 +33,26 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.awt.Font;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.SystemColor;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
 
 public class HistoryFrame extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel panel;
-	private JLabel lblSelectDate;
-	private JLabel lblSelectContact;
-	private JRadioButton rdbtnDate;
-	private JRadioButton rdbtnContact;
-	private JLabel lblSearchBy;
 	private final ButtonGroup buttonGroup = new ButtonGroup();
-	private JLabel lblDetails;
 	private JList list;
-	private JSpinner spinnerDate;
-	private JComboBox comboBoxContacts;
-		
+	private JLabel lblTaskName;
+	private JLabel lblCompany;
+	private JScrollPane scrollPane;
+	private JTextArea textAreaDescription;
+	private JLabel lblDoneTasks;
+	private JLabel lblTaskTitleFilled;
+	private JLabel lblCompanyFilled;
 
 	/**
 	 * Create the frame.
@@ -64,162 +69,115 @@ public class HistoryFrame extends JFrame {
 
 		setContentPane(contentPane);
 		contentPane.add(getPanel(), BorderLayout.CENTER);
-		showContacts();
 		setResizable(false);
-		
+		showDone();
 	}
 
 	private JPanel getPanel() {
 		if (panel == null) {
 			panel = new JPanel();
 			panel.setLayout(null);
-			panel.add(getLblSelectDate());
-			panel.add(getLblSelectContact());
-			panel.add(getRdbtnContact());
-			panel.add(getRdbtnDate());
-			panel.add(getLblSearchBy());
-			panel.add(getLblDetails());
 			panel.add(getList());
-			panel.add(getSpinnerDate());
-			panel.add(getComboBoxContacts());
+			panel.add(getLblTaskName());
+			panel.add(getLblCompany());
+			panel.add(getScrollPane());
+			panel.add(getLblDoneTasks());
+			panel.add(getLblTaskTitleFilled());
+			panel.add(getLblCompanyFilled());
 		}
 		return panel;
 	}
-	private JLabel getLblSelectDate() {
-		if (lblSelectDate == null) {
-			lblSelectDate = new JLabel("Select date:");
-			lblSelectDate.setFont(Fonts.candaraNormal);
-			lblSelectDate.setBounds(252, 40, 108, 16);
-		}
-		return lblSelectDate;
-	}
-	private JLabel getLblSelectContact() {
-		if (lblSelectContact == null) {
-			lblSelectContact = new JLabel("Select contact:");
-			lblSelectContact.setFont(Fonts.candaraNormal);
-			lblSelectContact.setBounds(252, 63, 108, 16);
-		}
-		return lblSelectContact;
-	}
-	private JRadioButton getRdbtnDate() {
-		if (rdbtnDate == null) {
-			rdbtnDate = new JRadioButton("Date");
-			rdbtnDate.setSelected(true);
-			rdbtnDate.setFont(Fonts.candaraNormal);
-			rdbtnDate.addChangeListener(new ChangeListener() {
-				public void stateChanged(ChangeEvent arg0) {
-					showDate();
-				}
-			});
-			
-			buttonGroup.add(rdbtnDate);
-			rdbtnDate.setBounds(22, 36, 127, 25);
-		}
-		return rdbtnDate;
-	}
-	private JRadioButton getRdbtnContact() {
-		if (rdbtnContact == null) {
-			rdbtnContact = new JRadioButton("Contact");
-			rdbtnContact.setFont(Fonts.candaraNormal);
-			rdbtnContact.addChangeListener(new ChangeListener() {
-				public void stateChanged(ChangeEvent arg0) {
-					showContact();
-				}
 
-			});
-			buttonGroup.add(rdbtnContact);
-			rdbtnContact.setBounds(22, 59, 127, 25);
-		}
-		return rdbtnContact;
-	}
-	private JLabel getLblSearchBy() {
-		if (lblSearchBy == null) {
-			lblSearchBy = new JLabel("Search by:");
-			lblSearchBy.setFont(Fonts.candaraNormal);
-			lblSearchBy.setBounds(22, 13, 75, 16);
-		}
-		return lblSearchBy;
-	}
-	private JLabel getLblDetails() {
-		if (lblDetails == null) {
-			lblDetails = new JLabel("Details:");
-			lblDetails.setFont(Fonts.candaraNormal);
-			lblDetails.setBounds(32, 110, 56, 16);
-		}
-		return lblDetails;
-	}
 	private JList getList() {
 		if (list == null) {
 			list = new JList();
-			list.setBackground(Color.LIGHT_GRAY);
+			list.addListSelectionListener(new ListSelectionListener() {
+				public void valueChanged(ListSelectionEvent arg0) {
+					showTaskDetails();
+				}
+			});
+			list.setBackground(SystemColor.text);
 			list.setFont(Fonts.candaraNormal);
-			list.setBounds(42, 129, 484, 168);
+			list.setBounds(28, 48, 203, 257);
 		}
 		return list;
 	}
-	private void showDate() {
-		if(rdbtnDate.isSelected()) {
-			spinnerDate.setVisible(true);
-			
-			comboBoxContacts.setVisible(false);
-			lblSelectContact.setForeground(Color.GRAY);
-		}else {
-			spinnerDate.setVisible(false);
-			comboBoxContacts.setVisible(false);
-			lblSelectContact.setForeground(Color.BLACK);
+
+
+	private JLabel getLblTaskName() {
+		if (lblTaskName == null) {
+			lblTaskName = new JLabel("Task title:");
+			lblTaskName.setFont(new Font("Candara", Font.PLAIN, 15));
+			lblTaskName.setBounds(271, 51, 85, 14);
 		}
+		return lblTaskName;
 	}
-	private void showContact() {
-		if(rdbtnContact.isSelected()) {
-			spinnerDate.setVisible(false);
-			comboBoxContacts.setVisible(true);
-			
-			comboBoxContacts.setEditable(true);
-			lblSelectDate.setForeground(Color.GRAY);
-		}else {
-			spinnerDate.setVisible(false);
-			comboBoxContacts.setVisible(false);
-			lblSelectDate.setForeground(Color.BLACK);
+
+	private JLabel getLblCompany() {
+		if (lblCompany == null) {
+			lblCompany = new JLabel("Company:");
+			lblCompany.setFont(new Font("Candara", Font.PLAIN, 15));
+			lblCompany.setBounds(271, 88, 111, 14);
 		}
+		return lblCompany;
 	}
-	private JSpinner getSpinnerDate() {
-		if (spinnerDate == null) {
-			spinnerDate = new JSpinner();
-			spinnerDate.addChangeListener(new ChangeListener() {
-				public void stateChanged(ChangeEvent arg0) {
-					String format = new SimpleDateFormat("yyyy/MM/dd").format(spinnerDate.getValue());
-					System.out.println(format);
-					searchByDate(format);
-				}
-			});
-			spinnerDate.setModel(new SpinnerDateModel(new Date(1525417200000L), null, new Date(1525590000000L), Calendar.DAY_OF_MONTH));
-			
-			spinnerDate.setBounds(358, 38, 127, 16);
-			
-			}
-		return spinnerDate;
+
+	private JScrollPane getScrollPane() {
+		if (scrollPane == null) {
+			scrollPane = new JScrollPane();
+			scrollPane.setBounds(261, 123, 283, 182);
+			scrollPane.setViewportView(getTextAreaDescription());
+		}
+		return scrollPane;
+	}
+
+	private JTextArea getTextAreaDescription() {
+		if (textAreaDescription == null) {
+			textAreaDescription = new JTextArea();
+			textAreaDescription.setBackground(SystemColor.control);
+			textAreaDescription.setBorder(new TitledBorder(
+					new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)),
+					"Description", TitledBorder.LEFT, TitledBorder.TOP, new Font("Candara", Font.PLAIN, 15),
+					new Color(0, 0, 0)));
+		}
+		return textAreaDescription;
+	}
+
+	private JLabel getLblDoneTasks() {
+		if (lblDoneTasks == null) {
+			lblDoneTasks = new JLabel("Done tasks:");
+			lblDoneTasks.setFont(new Font("Candara", Font.PLAIN, 16));
+			lblDoneTasks.setBounds(28, 22, 97, 14);
+		}
+		return lblDoneTasks;
+	}
+
+	private JLabel getLblTaskTitleFilled() {
+		if (lblTaskTitleFilled == null) {
+			lblTaskTitleFilled = new JLabel("");
+			lblTaskTitleFilled.setBounds(350, 51, 210, 14);
+		}
+		return lblTaskTitleFilled;
+	}
+
+	private JLabel getLblCompanyFilled() {
+		if (lblCompanyFilled == null) {
+			lblCompanyFilled = new JLabel("");
+			lblCompanyFilled.setBounds(350, 88, 210, 14);
+		}
+		return lblCompanyFilled;
 	}
 	
-	private JComboBox getComboBoxContacts() {
-		if (comboBoxContacts == null) {
-			comboBoxContacts = new JComboBox();
-			comboBoxContacts.setBounds(358, 58, 127, 16);
-		}
-		return comboBoxContacts;
+	public void showDone() {
+		list.setListData(GUIKontroler.showDoneTasks().toArray());
 	}
 	
-	public void showContacts() {
-		for(int i = 0; i < GUIKontroler.showAllContacts().size(); i++)
-			comboBoxContacts.addItem(GUIKontroler.showAllContacts().get(i));
-	}
-	public GregorianCalendar searchByDate(String s) {
-		int br = s.lastIndexOf("/");
-		int dd = Integer.parseInt(s.substring(br + 1,s.length()));
-		//int br1 = s.indexOf("/");
-		int mm = Integer.parseInt(s.substring(6, 7));
-		int yy = Integer.parseInt(s.substring(0, 4)); 
-		//System.out.println("Datum: "+ dd+"."+mm+"."+yy+".");
-		return new GregorianCalendar(yy, mm, dd);
-		
+	private void showTaskDetails() {
+		Task task = (Task)list.getSelectedValue();
+		if (task!=null) {
+			lblCompanyFilled.setText(task.getCoWorker().getCompanyName());
+			lblTaskTitleFilled.setText(task.getTaskTitle());
+			textAreaDescription.setText(task.getDescription());
+		}
 	}
 }
